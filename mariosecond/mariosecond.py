cdee4fd5ddd7608fbe5c0ba622e14c903cd6328c
ShaokunJiang <116729650+ShaokunJiang@users.noreply.github.com>
2023-03-14 20:54:57 -0400

    Update mariosecond.py

--- mariosecond/mariosecond.py
+++ mariosecond/mariosecond.py
@@ -1,8 +1,8 @@
 import pygame, sys
-from setting import *
+from settings import *
 from level import Level
 from main import overworld
-from UI import ui
+from ui import UI
 
 class Game:
     def __init__(self):
@@ -12,29 +12,58 @@ class Game:
         self.cur_health = 100
         self.coins = 0
 
-        #pverworld creation
+        #audio
+        self.level_bg_musci = pygame.mixer.Sound('C:\\Users\\shaok\\source\\repos\\2-7-2023 cmp hw\\2DMariostyleplatformer\\audio\\level_music.wav')
+        self.overworld_bg_music = pygame.mixer.Sound('C:\\Users\\shaok\\source\\repos\\2-7-2023 cmp hw\\2DMariostyleplatformer\\audio\\overworld_music.wav') 
+
+        #overworld creation
         self.overworld = overworld(0,self.max_level,screen,self.create_level)
         self.status = 'overworld'
-
+        self.overworld_bg_music.play(loops = -1)
         # user interface
-        self.UI = ui(screen)
+        self.ui = UI(screen)
+
+        
 
     def create_level(self,current_level):
-        self.level = Level(current_level,screen,self.create_overworld)
+        self.level = Level(current_level,screen,self.create_overworld,self.change_coins,self.change_health)
         self.status = 'level'
-
+        self.overworld_bg_music.stop()
+        self.level_bg_musci.play(loops = -1)
+    
     def create_overworld(self,current_level,new_max_level):
         if new_max_level >self.max_level:
             self.max_level = new_max_level
         self.overworld = overworld(current_level,self.max_level,screen,self.create_level)
-        self.status = 'overworld'    
+        self.status = 'overworld' 
+        self.overworld_bg_music.play(loops = -1)
+        self.level_bg_musci.stop()
+        
+    def change_coins(self,amount):
+        self.coins += amount 
+
+
+    def change_health(self,amount):
+        self.cur_health += amount
+
+
+    def check_game_over(self):
+        if self.cur_health <=0:
+            self.cur_health = 100
+            self.coins = 0
+            self.max_level = 0
+            self.overworld = overworld(0,self.max_level,screen,self.create_level)
+            self.status = 'overworld'
+            self.level_bg_musci.stop()
+            self.overworld_bg_music.play(loops = -1)
     def run(self):
         if self.status == 'overworld':
             self.overworld.run()
         else:
             self.level.run()
-            self.UI.show_health(50,100)
-            self.UI.show_coins(self.coins)
+            self.ui.show_health(self.cur_health,self.max_health)
+            self.ui.show_coins(self.coins)
+            self.check_game_over()
 #pygame setup
 pygame.init()
 screen=pygame.display.set_mode((screen_width,screen_height))
